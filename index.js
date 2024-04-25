600206c92d5f9b1004bf71ad9d087039d01fa5e1
Dave Banwell <dave@davebanwell.com>
2024-04-25 02:29:11 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Dave Banwell",
+    streetAddress: "9099 Riverside Dr. E.",
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {...employee, [key]: value,};
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
